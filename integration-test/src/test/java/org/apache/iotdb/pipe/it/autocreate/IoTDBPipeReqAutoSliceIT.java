ebcdf915f81fc8e71b7e79bf6aa3fc7d12885c70
luoluoyuyu <zhenyu@apache.org>
2024-08-29 09:50:05 +0800

    modify code format

--- integration-test/src/test/java/org/apache/iotdb/pipe/it/autocreate/IoTDBPipeReqAutoSliceIT.java
+++ integration-test/src/test/java/org/apache/iotdb/pipe/it/autocreate/IoTDBPipeReqAutoSliceIT.java
@@ -2,6 +2,7 @@ package org.apache.iotdb.pipe.it.autocreate;
 
 import org.apache.iotdb.it.framework.IoTDBTestRunner;
 import org.apache.iotdb.itbase.category.MultiClusterIT2AutoCreateSchema;
+
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
@@ -9,12 +10,9 @@ import org.junit.runner.RunWith;
 @Category({MultiClusterIT2AutoCreateSchema.class})
 public class IoTDBPipeReqAutoSliceIT extends AbstractPipeDualAutoIT {
 
-
-    @Override
-    public void setUp() {
-        super.setUp();
-        senderEnv.getConfig()
-                .getDataNodeCommonConfig()
-                .setPipeMetaSyncerInitialSyncDelayMinutes();
-    }
+  @Override
+  public void setUp() {
+    super.setUp();
+    senderEnv.getConfig().getDataNodeCommonConfig().setPipeMetaSyncerInitialSyncDelayMinutes();
+  }
 }
